9b656d3f2677d52b581df61fccc735d903799b8d
fatin-nadhirah <fatin.nadhirah@axa.com.my>
2021-12-24 22:51:28 +0800

    Add gitignore and .env

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express")
 const mongoose = require("mongoose") // new
 const create = require("./createBooking") // new
 const update = require("./editBooking")
+const dotenv = require('dotenv')
+dotenv.config();
 
-
+console.log("url::",process.env.ATLAS_URI);
 // Connect to MongoDB database
 mongoose
-	.connect("mongodb+srv://NadhirahM:harihDanNitaf@cluster0.pxlv8.mongodb.net/Booking?retryWrites=true&w=majority", { useNewUrlParser: true })
+	.connect(process.env.ATLAS_URI, { useNewUrlParser: true })
 	.then(() => {
 		const app = express()
 		app.use(express.json()) // new 
@@ -14,7 +16,7 @@ mongoose
         app.use("/api", create)
         app.use("/api", update)
 
-		app.listen(5000, () => {
+		app.listen(process.env.PORT, () => {
 			console.log("Server has started!")
 		})
 	})
